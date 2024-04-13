aa1b56c6ae5eecfdd134982a48739a9c0ad949a3
Nicolas Brandao <nicolas.brandaor@gmail.com>
2024-04-13 11:34:12 -0300

    FIX: remove AuthModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,9 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
 import { UsersModule } from './users/users.module';
-import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [DatabaseModule, UsersModule, AuthModule],
+  imports: [DatabaseModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
