07686f68ba16283f5d90005a3e8bc5b38882eed2
Nicolas Brandao <nicolas.brandaor@gmail.com>
2024-04-13 19:18:38 -0300

    FEA: implement base CORS

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({ origin: process.env.ALLOWED_ORIGIN });
   await app.listen(3000);
 }
 bootstrap();
