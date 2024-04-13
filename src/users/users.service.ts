9a8bf472e66d45dc4804f95f552941ed11bb5fed
Nicolas Brandao <nicolas.brandaor@gmail.com>
2024-04-12 22:45:09 -0300

    FEA: create Users and Database resources

---
+++ src/users/users.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@nestjs/common';
+import { Prisma } from '@prisma/client';
+import { DatabaseService } from 'src/database/database.service';
+
+@Injectable()
+export class UsersService {
+  constructor(private readonly databaseService: DatabaseService) {}
+
+  async create(createUserDto: Prisma.UserCreateInput) {
+    return this.databaseService.user.create({ data: createUserDto });
+  }
+
+  async findAll() {
+    return this.databaseService.user.findMany({});
+  }
+
+  async findOne(id: string) {
+    return this.databaseService.user.findUnique({ where: { id } });
+  }
+
+  update(id: string, updateUserDto: Prisma.UserUpdateInput) {
+    return this.databaseService.user.update({
+      where: { id },
+      data: updateUserDto,
+    });
+  }
+
+  remove(id: string) {
+    return this.databaseService.user.delete({ where: { id } });
+  }
+}
