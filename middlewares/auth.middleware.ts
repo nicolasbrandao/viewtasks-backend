110c3854befb7922eda132e9312ad4434808c9bd
Nicolas Brandao <nicolas.brandaor@gmail.com>
2024-04-14 13:36:24 -0300

    ENH: improve cookies and header logic

--- middlewares/auth.middleware.ts
+++ middlewares/auth.middleware.ts
@@ -4,9 +4,8 @@ import { Request, Response, NextFunction } from 'express';
 @Injectable()
 export class AuthMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: NextFunction) {
-    const accessToken = req.cookies['access_token'];
-
-    if (accessToken) {
+    if (req.cookies && req.cookies['access_token']) {
+      const accessToken = req.cookies['access_token'];
       req.headers['Authorization'] = `Bearer ${accessToken}`;
     }
 
