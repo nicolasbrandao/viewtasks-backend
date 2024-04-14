12f6ff809c3e650f161defd9cded031bf501c1fb
Nicolas Brandao <nicolas.brandaor@gmail.com>
2024-04-13 22:32:48 -0300

    FEA: create auth middleware

---
+++ middlewares/auth.middleware.ts
@@ -0,0 +1,15 @@
+import { Injectable, NestMiddleware } from '@nestjs/common';
+import { Request, Response, NextFunction } from 'express';
+
+@Injectable()
+export class AuthMiddleware implements NestMiddleware {
+  use(req: Request, res: Response, next: NextFunction) {
+    const accessToken = req.cookies['access_token'];
+
+    if (accessToken) {
+      req.headers['Authorization'] = `Bearer ${accessToken}`;
+    }
+
+    next();
+  }
+}
