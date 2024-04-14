d907dcf74dd29b837ee256370876709afb63e6b8
Nicolas Brandao <nicolas.brandaor@gmail.com>
2024-04-13 22:36:21 -0300

    REF: remove cors temporarily

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
-
-const corsOptions: CorsOptions = {
-  origin: process.env.ALLOWED_ORIGIN,
-};
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors(corsOptions);
   await app.listen(3000);
 }
 bootstrap();
