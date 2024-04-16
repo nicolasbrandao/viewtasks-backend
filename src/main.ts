445f7c41cf405f84f558d6c76f7064aa111f9b78
Nicolas Brandao <nicolas.brandaor@gmail.com>
2024-04-16 17:47:54 -0300

    FEA: implement swagger

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 const corsOptions: CorsOptions = {
   origin: process.env.ALLOWED_ORIGIN,
@@ -14,6 +15,16 @@ const listenCb = () => {
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Just To-Do it')
+    .setDescription('Task Management App')
+    .setVersion('1.0')
+    .addTag('To-Do')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
   app.enableCors(corsOptions);
   await app.listen(port, host, listenCb);
 }
